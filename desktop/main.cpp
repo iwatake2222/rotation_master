6ec00c54a75b532efd8fe145e1259f3e4052137c
iwatake2222 <take.iwiw2222@gmail.com>
2022-01-06 17:48:24 +0900

    Modify cmake settings to use ImGui and add sample code. issue #8

--- desktop/main.cpp
+++ desktop/main.cpp
@@ -22,6 +22,11 @@ limitations under the License.
 #include <GL/glew.h>     /* this must be before including glfw*/
 #include <GLFW/glfw3.h>
 
+/* for ImGui */
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 /*** Macro ***/
 /* macro functions */
 #define RUN_CHECK(x)                                         \
@@ -55,17 +60,84 @@ int main(int argc, char *argv[])
     /* Ensure not to miss input */
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-    /* Dark blue background */
-    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-    
+    /* Initialize ImGui */
+    /* imgui:  Setup Dear ImGui context */
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    /* imgui:  Setup Dear ImGui style */
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    /* imgui:  Setup Platform/Renderer backends */
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    /* imgui: state */
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
     /*** Start loop ***/
     while(1) {
+        glfwPollEvents();
+
+        /* imgui:  Start the Dear ImGui frame */
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
         /* Clear the screen */
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /* imgui:  Rendering */
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         /* Swap buffers */
         glfwSwapBuffers(window);
-        glfwPollEvents();
 
         /* Check if the ESC key was pressed or the window was closed */
         if (glfwWindowShouldClose(window) != 0 || glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
